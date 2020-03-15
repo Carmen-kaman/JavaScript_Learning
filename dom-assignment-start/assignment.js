30c6fe94150738f96cea90293219a39302daca67
Carmen-kaman <cahejiamin@163.com>
2020-03-15 15:19:12 -0400

    DOM assignment
    my solution

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,21 @@
+// Q1-1
+const lists = document.querySelectorAll("li");
+for (const item of lists) {
+  item.style.backgroundColor = "black";
+  item.style.color = "white";
+}
+
+// Q1-2
+const lists = document.getElementsByTagName("li");
+for (const item of lists) {
+  item.style.backgroundColor = "pink";
+  item.style.color = "white";
+}
+
+// Q2-1
+const title = document.querySelector("head title");
+title.text = "Assignment - Solved !!!";
+
+// Q3
+const h1 = document.querySelector("h1");
+h1.textContent = "Assignment - Solved !!!";
