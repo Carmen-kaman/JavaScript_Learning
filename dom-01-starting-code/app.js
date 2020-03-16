1d06922aa5634ad13f1855ef8ed645aa08c9fb5f
Carmen-kaman <cahejiamin@163.com>
2020-03-16 12:49:08 -0400

    style DOM Element

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -2,3 +2,19 @@ const ul = document.body.firstElementChild.nextElementSibling;
 const firstLi = ul.firstElementChild;
 
 console.log(firstLi);
+
+const section = document.querySelector("section");
+const buttton = document.querySelector("button");
+// section.style.backgroundColor = "blue";
+section.className = "red-bg";
+
+buttton.addEventListener("click", () => {
+  // if (section.className === "red-bg visible") {
+  //   section.className = "red-bg invisible";
+  // } else {
+  //   section.className = "red-bg visible";
+  // }
+
+  // section.classList.toggle("visible");
+  section.classList.toggle("invisible");
+});
