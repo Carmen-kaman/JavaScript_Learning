f0bde33bb4e5d9d8a5fde163538195cf36e71163
Carmen-kaman <cahejiamin@163.com>
2020-03-28 20:39:31 -0400

    demo project set up
    finish add movie button

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,26 +1,58 @@
-const movieList = document.getElementById("movie-list");
-movieList.style["backgroundColor"] = "red";
-movieList.style.display = "block";
+// const movieList = document.getElementById("movie-list");
+// movieList.style["backgroundColor"] = "red";
+// movieList.style.display = "block";
 
-const userChosenName = "level";
+// const userChosenName = "level";
 
-let person = {
-  "first name": "Max",
-  age: 30,
-  hobbies: ["Sport", "Cooking"],
-  [userChosenName]: "ccMax",
-  greet: function() {
-    alert("Hi there!!!!");
+// let person = {
+//   "first name": "Max",
+//   age: 30,
+//   hobbies: ["Sport", "Cooking"],
+//   [userChosenName]: "ccMax",
+//   greet: function() {
+//     alert("Hi there!!!!");
+//   }
+// };
+
+// // adding, modifying & deleting Properties
+// person.isAdmin = true;
+
+// delete person.age;
+
+// console.log(person);
+
+// // properties access
+
+// console.log(person["first name"]);
+
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movie = [];
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
   }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movie.push(newMovie);
+  console.log(newMovie);
 };
 
-// adding, modifying & deleting Properties
-person.isAdmin = true;
-
-delete person.age;
-
-console.log(person);
-
-// properties access
-
-console.log(person["first name"]);
+addMovieBtn.addEventListener("click", addMovieHandler);
