ea725de8c60cd8d96c9687bbe92f2af4b19d7852
Carmen-kaman <cahejiamin@163.com>
2020-04-09 15:07:30 -0400

    switch logic

---
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,48 @@
+class Tooltip {}
+
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type"); // choose last button
+    switchBtn.addEventListener("click");
+  }
+}
+
+class ProjectList {
+  projects = [];
+  constructor(type, switchHandlerFunction) {
+    this.type = type;
+    this.switchHandler = switchHandlerFunction;
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    // console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+  }
+}
+
+App.init();
