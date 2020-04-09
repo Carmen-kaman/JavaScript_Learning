b60bdd33cadad492e1efb564a8a3706963a7eec8
Carmen-kaman <cahejiamin@163.com>
2020-04-08 21:02:31 -0400

    settting & getting prototype

--- prototypes-01-starting-setup/app.js
+++ prototypes-01-starting-setup/app.js
@@ -18,13 +18,13 @@
 // }
 
 // construtor function
-function Person() {
-  this.age = 30;
-  this.name = "Json";
-  this.greet = function () {
-    console.log(`Hi, I am ${this.name}, and I am ${this.age} years old.`);
-  };
-}
+// function Person() {
+//   this.age = 30;
+//   this.name = "Json";
+//   this.greet = function () {
+//     console.log(`Hi, I am ${this.name}, and I am ${this.age} years old.`);
+//   };
+// }
 
 // Person.prototype = {
 //   printAge() {
@@ -32,16 +32,63 @@ function Person() {
 //   },
 // };
 
-Person.prototype.printAge = function () {
-  console.log(this.age);
+// Person.prototype.printAge = function () {
+//   console.log(this.age);
+// };
+
+// console.dir(Person);
+
+// const person = new Person();
+// person.greet();
+// person.printAge();
+// console.log(person.__proto__);
+// console.log(person.toString());
+
+// const p2 = new person.__proto__.constructor();
+// console.log(p2);
+
+const course = {
+  // new object
+  title: "JS",
+  rating: 5,
 };
 
-console.dir(Person);
+console.log(Object.getPrototypeOf(course));
+console.log(course.__proto__);
 
-const person = new Person();
-person.greet();
-person.printAge();
-console.log(person.__proto__);
+Object.setPrototypeOf(course, {
+  printRating: function () {
+    console.log(`${this.rating}/5`);
+  },
+});
 
-const p2 = new person.__proto__.constructor();
-console.log(p2);
+course.printRating();
+
+const student = Object.create(
+  {
+    printProgress: function () {
+      console.log(this.progress);
+    },
+  },
+  {
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: "Max",
+      writable: true,
+    },
+  }
+);
+
+// adding property by different methods in empty object
+student.name = "Janey";
+
+Object.defineProperty(student, "progress", {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false,
+});
+
+student.printProgress();
+console.log(student);
