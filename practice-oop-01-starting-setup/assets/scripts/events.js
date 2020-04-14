6597c6ea3bda7e5bb317c677cc033280a935e138
Carmen-kaman <cahejiamin@163.com>
2020-04-13 21:28:50 -0400

    remove event listener

--- practice-oop-01-starting-setup/assets/scripts/events.js
+++ practice-oop-01-starting-setup/assets/scripts/events.js
@@ -12,5 +12,13 @@ const anotherButtonCLick = () => {
   console.log("this is another click!!");
 };
 
-button.onclick = onButtonClickHandler;
-button.onclick = anotherButtonCLick; // the first one onclike function never execute
+// button.onclick = onButtonClickHandler;// never execute
+// button.onclick = anotherButtonCLick; // the first one onclike function never execute
+
+const btnBind = onButtonClickHandler.bind(this);
+
+button.addEventListener("click", btnBind);
+
+setTimeout(() => {
+  button.removeEventListener("click", btnBind);
+}, 2000); // cannot remove arrow fuction in add even listener
